2367a8cb1f07159647ff902eb134fc05826b1d17
Dmitriy Batogov <hi@batogov.ru>
2017-10-24 20:29:16 +0300

    Добавил класс игры, функции для обновления состояния игры и отрисовки. Добавил в игру мяч и двух игроков

--- pong.js
+++ pong.js
@@ -34,3 +34,95 @@ class Player extends Rect {
         this.score = 0;
     }
 }
+
+class Pong {
+    constructor(canvas) {
+        this._canvas = canvas;
+        this._context = canvas.getContext('2d');
+
+        this._accumulator = 0;
+        this.step = 1 / 120;
+
+        // Создаём мяч и добавляем ему необходимые свойства
+        this.ball = new Ball;
+        this.ball.velocity.x = 200;
+        this.ball.velocity.y = 200;
+        this.ball.pos.x = 100;
+        this.ball.pos.y = 100;
+
+        // Массив игроков
+        this.players = [
+            new Player,
+            new Player
+        ];
+
+        // Позиционируем игроков
+        this.players[0].pos.x = 30;
+        this.players[1].pos.x = this._canvas.width - 30;
+        this.players.forEach(player => {
+            player.pos.y = this._canvas.height / 2;
+        })
+
+        // Рекурсивная функция, которая обновляет состояние игры
+        // и отрисовывает новый кадр
+        let lastTime;
+        const callback = (millis) => {
+            if (lastTime) {
+                this.update((millis - lastTime) / 1000);
+                this.draw();
+            }
+            lastTime = millis;
+            requestAnimationFrame(callback);
+        }
+
+        callback();
+    }
+
+    drawRect(rect) {
+        this._context.fillStyle = '#fff';
+        this._context.fillRect(
+            rect.left, rect.top,
+            rect.size.x, rect.size.y
+        );
+    }
+
+    /**
+     * Функция отрисовывает новый кадр игры.
+     */
+    draw() {
+        this._context.fillStyle = '#000';
+        this._context.fillRect(0, 0, canvas.width, canvas.height);
+
+        this.drawRect(this.ball);
+        this.players.forEach(player => this.drawRect(player));
+    }
+
+    simulate(step) {
+        this.ball.pos.x += this.ball.velocity.x * step;
+        this.ball.pos.y += this.ball.velocity.y * step;
+
+        if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
+            this.ball.velocity.x = -this.ball.velocity.x;
+        }
+
+        if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
+            this.ball.velocity.y = -this.ball.velocity.y;
+        }
+    }
+
+    /**
+     * Функция обновления состояния игры. Внутри себя необходимое количество
+     * раз вызывает функцию simulate.
+     * @param {number} dt Время, прошедшее с прошлого обновления.
+     */
+    update(dt) {
+        this._accumulator += dt;
+        while (this._accumulator > this.step) {
+            this.simulate(this.step);
+            this._accumulator -= this.step;
+        }
+    }
+}
+
+const canvas = document.getElementById('pong');
+const pong = new Pong(canvas);
